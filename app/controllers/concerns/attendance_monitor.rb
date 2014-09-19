6c9d301f0d1cfee55f0d96924474b307cca29867
hayduke19us <hayduke19us@gmail.com>
2014-09-19 00:56:19 -0600

    refactor JSON.parse and Net:HTTP methods

--- app/controllers/concerns/attendance_monitor.rb
+++ app/controllers/concerns/attendance_monitor.rb
@@ -8,18 +8,24 @@ module AttendanceMonitor
   KEY = ENV["meetup_api_key"]
   GROUP = "momsmeetupofco"
 
+  def get uri_object
+    Net::HTTP.get_response(uri_object)
+  end
+
+  def parse body
+    JSON.parse(body)
+  end
+
   def members_info
     uri = URI.parse("#{BASE}2/members?sign=true&key=#{KEY}&group_urlname=#{GROUP}")
-    http = Net::HTTP.new(uri.host, uri.port)
-    https_setup(http)
-    request = Net::HTTP::GET.new(uri.request_uri)
-    response = JSON.parse(response.body)
+    response = get(uri)
+    response = parse(response.body)
   end
 
   def members_count
     uri = URI.parse("#{BASE}2/groups?sign=true&key=#{KEY}&group_urlname=#{GROUP}")
-    response = Net::HTTP.get_response(uri)
-    response = JSON.parse(response.body) 
+    response = get(uri)
+    response = parse(response.body)
     members ||= response["results"].first["members"]
   end
 
